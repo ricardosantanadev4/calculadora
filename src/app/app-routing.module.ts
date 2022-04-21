b698909ad36fdf408affa21cbb8044a91fd41096
ricardosantanadev4 <ricardosantanadev4@gmail.com.br>
2022-04-21 12:32:18 -0300

    calculadora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calculadora', component: CalculadoraComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
